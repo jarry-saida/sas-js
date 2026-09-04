ffcf347a7ad87aca39a8d8fc02402416b0d448ff
jarry-saida <jarrysaida@gmail.com>
2026-09-04 17:41:48 +0200

    Refactor and add new functions in challenges3.js

--- sas-challenges/Day3/challenges3.js
+++ sas-challenges/Day3/challenges3.js
@@ -52,7 +52,7 @@ inversionChaine("saida");
 //EX3
 function contChar(str, char){
     let comp = 0 ;
-    for(i=0;i<char.length;i++){
+    for(let i=0;i<char.length;i++){
         if(char[i].toUpperCase() === str.toUpperCase()){
             comp++;
         }
@@ -60,3 +60,30 @@ function contChar(str, char){
     console.log("le nombre de str est : "+ comp);
 }
 contChar("H","hmjgfhfhf");
+//NIVEAU 3 
+//EX1
+function MiseEnMajusculesDesMots(phrase1){
+    let phrase2 = phrase1.split(" ");
+    for(let i=0 ;i < phrase2.length; i++){
+        let phrase3 = phrase2[i];
+        phrase2[i] = phrase2[i][0].toUpperCase() + phrase2[i].slice(1).toLowerCase();
+    }
+    let phrase3 = phrase2.join(" ");
+    console.log("la nouveau phrase est :" + phrase3);
+}
+MiseEnMajusculesDesMots("saida Jarry dE agadiR");
+//EX2
+function CompresseurDeChines (caractere){
+    let comp = 1;
+    let resul = "";
+    for(let i = 0 ; i< caractere.length ; i++){
+        if( caractere[i] ===  caractere[i+1]) {
+            comp++;
+        }else{
+            resul += caractere[i] + comp  ;
+            comp = 1;
+        }
+    } 
+    console.log("le caractere compresse est :" + resul);
+}
+CompresseurDeChines ("aaabbc");
